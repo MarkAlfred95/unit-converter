4203a4fdc77061f13953c19ee9b56c1b43b5e5e2
MarkAlfred95 <markalfredtornado@gmail.com>
2024-03-12 17:36:16 +0800

    description

--- src/App.js
+++ src/App.js
@@ -13,10 +13,13 @@ import NotFound from './pages/NotFound';
 
 import AgeCalculator from './pages/calculator/age_calculator';
 import AreaCalculator from './pages/calculator/area_calculator/area_calculator';
+import AverageCalculator from './pages/calculator/average_calculator';
 import BMICalculator from './pages/calculator/bmi_calculator';
 import DataConverter from './pages/converter/data_converter';
+import DiscountCalculator from './pages/calculator/discount_calculator';
 import LengthConverter from './pages/converter/length_converter';
 import MassConverter from './pages/converter/mass_converter';
+import PercentageCalculator from './pages/calculator/percentage_calculator';
 import SpeedConverter from './pages/converter/speed_converter';
 import TemperatureConverter from './pages/converter/temperature_converter';
 import TimeConverter from './pages/converter/time_converter';
@@ -35,10 +38,13 @@ const router = createBrowserRouter(
       <Route path="calculator" element={<CalculatorLayout />}>
         <Route path="age-calculator" element={<AgeCalculator />} />
         <Route path="area-calculator" element={<AreaCalculator />} />
+        <Route path="average-calculator" element={<AverageCalculator />} />
         <Route path="bmi-calculator" element={<BMICalculator />} />
         <Route path="data-converter" element={<DataConverter />} />
+        <Route path="discount-calculator" element={<DiscountCalculator />} />
         <Route path="length-converter" element={<LengthConverter />} />
         <Route path="mass-converter" element={<MassConverter />} />
+        <Route path="percentage-calculator" element={<PercentageCalculator />} />
         <Route path="speed-converter" element={<SpeedConverter />} />
         <Route path="temperature-converter" element={<TemperatureConverter />} />
         <Route path="time-converter" element={<TimeConverter />} />
---
+++ src/pages/calculator/discount_calculator.js
@@ -0,0 +1,151 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+import CustomButton from '../../components/custom/custom_button';
+import CustomBtnClear from '../../components/custom/custom_btn_clear';
+
+const DiscountCalculator = () => {
+    const [isPercent, setIsPercent] = useState(true);
+    const [priceBeforeDiscount, setPriceBeforeDiscount] = useState(0);
+    const [discountValue, setDiscountValue] = useState(0);
+    const [priceAfterDiscount, setPriceAfterDiscount] = useState(0);
+    const [savedAmount, setSavedAmount] = useState(0);
+    const [discountPercentage, setDiscountPercentage] = useState(0);
+
+    const calculateDiscount = () => {
+        if (isPercent) {
+            const calculatedDiscount = (priceBeforeDiscount * discountValue) / 100;
+            const priceAfterDiscount = priceBeforeDiscount - calculatedDiscount;
+            setPriceAfterDiscount(priceAfterDiscount);
+            setSavedAmount(calculatedDiscount);
+            setDiscountPercentage(discountValue);
+        }
+        else {
+            const priceAfterDiscount = priceBeforeDiscount - discountValue;
+            const calculatedDiscountPercentage = ((priceBeforeDiscount - priceAfterDiscount) / priceBeforeDiscount) * 100;
+            setPriceAfterDiscount(priceAfterDiscount);
+            setSavedAmount(discountValue);
+            setDiscountPercentage(calculatedDiscountPercentage);
+        }
+    };
+
+    const discountResult = [
+        {result_name: "Price After Discount", result: priceAfterDiscount},
+        {result_name: "Discount Percentage", result: discountPercentage + "%"},
+        {result_name: "Amount Saved", result: savedAmount},
+    ]
+
+    const handleClear = () => {
+        setPriceBeforeDiscount('');
+        setDiscountValue('');
+        setPriceAfterDiscount(0);
+        setSavedAmount(0);
+        setDiscountPercentage(0);
+    }
+
+    return (
+        <>
+        <div className="pages_wrap">
+            <div className="pages_container">
+                <div className="pages_description_wrap">
+                    <h1>Discount Calculator</h1>
+                    <p>
+                        A Discount Calculator is a handy tool used to determine the final price of a product or 
+                        service after applying a discount or markdown. It enables users to swiftly compute the 
+                        discounted price by inputting the original price and the discount percentage or amount. 
+                    </p>
+                </div>
+                <div className="calc_container">
+                    <div className="discount_calc_wrap">
+                        <div className="discount_calc_inputs_wrap">
+                            <div className="bmi_unit_system_select">
+                                <button
+                                    onClick={() => setIsPercent(true)}
+                                    style={{
+                                        backgroundColor: isPercent ? '#1B262C' : 'white',
+                                        color: isPercent ? 'white' : '#1B262C',
+                                    }}
+                                >Percent Off
+                                </button>
+                                <button
+                                    onClick={() => setIsPercent(false)}
+                                    style={{
+                                        backgroundColor: !isPercent ? '#1B262C' : 'white',
+                                        color: !isPercent ? 'white' : '#1B262C',
+                                    }}
+                                >Fixed Amount Off
+                                </button>
+                            </div>
+                            <div className="discount_input_wrap">
+                                <p>Price Before Discount:</p>
+                                <input
+                                    type="number"
+                                    placeholder="0"
+                                    value={priceBeforeDiscount}
+                                    onChange={(e) => setPriceBeforeDiscount(parseFloat(e.target.value))}
+                                />
+                            </div>
+                            <div>
+                                {isPercent && 
+                                    <motion.div 
+                                        className="discount_input_wrap"
+                                        initial={{ opacity: 0, x: "-5rem" }}
+                                        animate={{ opacity: 1, x: 0 }}
+                                    >
+                                        <p>Discount %:</p>
+                                        <input
+                                            type="number"
+                                            placeholder="0"
+                                            value={discountValue}
+                                            onChange={(e) => setDiscountValue(parseFloat(e.target.value))}
+                                        />
+                                    </motion.div>
+                                }
+                                {!isPercent &&
+                                    <motion.div 
+                                        className="discount_input_wrap"
+                                        initial={{ opacity: 0, x: "-5rem" }}
+                                        animate={{ opacity: 1, x: 0 }}
+                                    >
+                                        <p>Discount Amount:</p>
+                                        <input
+                                          type="number"
+                                          placeholder="0"
+                                          value={discountValue}
+                                          onChange={(e) => setDiscountValue(parseFloat(e.target.value))}
+                                        />
+                                    </motion.div>
+                                }
+                            </div>
+                            <div className="bmi_btn_wrap">
+                                <CustomButton
+                                    btn_name={"Calculate"}
+                                    click={calculateDiscount} 
+                                />
+                                <CustomBtnClear 
+                                    clear={handleClear}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="calc_table_wrap">
+                            <table className="bmi_table">
+                                <tbody>
+                                  {discountResult.map((row, index) => (
+                                    <tr key={index}>
+                                      <td>{row.result_name}</td>
+                                      <td>{row.result}</td>
+                                    </tr>
+                                  ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default DiscountCalculator;
